c3a4820081f18e97616dcb825c0d12e83b3d0ccf
aadlani mohammed <aadlani9@gmail.com>
2022-03-31 17:12:58 +0000

    commit 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,40 +1,27 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {decrementAction, incrementAction, incrementByValue} from "../redux/actions/counterActions";
+import {decrement, increment} from "../redux/counterSlice";
 
 export default function Counter (){
-    // const [count , setCount] = useState(0);
-    // const {countState} = useSelector(function (state){
-    //      return state.count
-    // });
-    //const countState = useSelector(state => state.count);
-    // -- Or --
-    const {count} = useSelector(state => state.counter);  // state.counter // key in index.js
+    const count = useSelector(state => state.counter);
     const dispatch = useDispatch();
 
     console.log(count)
 
     const handleIncrement = () => {
-        //setCount(count + 1) ;
-        // dispatch(incrementAction())
-        incrementAction(dispatch);
+        // incrementAction(dispatch);
+        dispatch(increment)
     }
+
     const handleDecrement = () => {
-        // setCount(count -1 )
-        // dispatch(decrementAction());
-        decrementAction(dispatch)
+        // decrementAction(dispatch)
+        dispatch(decrement)
     }
 
     const handleIncByValue = (v1) => {
-        //dispatch(incrementByValue(v1))
         incrementByValue(v1,dispatch)
     }
-
-    // const handleIncByValue = () => {
-    //     dispatch({
-    //         type: INC_BY_VALUE
-    //     })
-    // }
 
     return (
         <div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
 import {createStore, applyMiddleware} from "redux";
 import reducers from './reducers/index'
 import reduxThunk from 'redux-thunk'
+import {configureStore} from "@reduxjs/toolkit";
+import counterReducer from "./reducers/counterReducer";
+import {counterSlice} from "./counterSlice";
 
-const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+// const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+// const store = createStore(reducers, enhancer(applyMiddleware(reduxThunk)))
 
-
-const store = createStore(reducers, enhancer(applyMiddleware(reduxThunk)))
+const store = configureStore({
+    reducer: {
+        // counter : counterReducer,
+        counter : counterSlice, //  counterSlice => Inside it: [Action and Reducer]
+    }
+})
 
 export default store
